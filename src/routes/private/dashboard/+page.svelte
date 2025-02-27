f46dbd46084cf17ea81c3323fa47a0b8a5df330b
WKogut98 <wojciech.kogut7@gmail.com>
2025-02-27 20:17:59 +0100

    private dashboard

--- src/routes/private/dashboard/+page.svelte
+++ src/routes/private/dashboard/+page.svelte
@@ -1,17 +1,50 @@
 <script lang="ts">
     import { getUserState } from "$lib/state/user-state.svelte";
+    import Icon from "@iconify/svelte";
 
     let userContext = getUserState();
-    let {allBooks} = $derived(userContext);
+    let {userName, allBooks} = $derived(userContext);
 
     $inspect(allBooks);
 </script>
 
-<ul>
-{#each allBooks as book}
-    <li>
-        <h4>{book.title}</h4>
-        <p>{book.author}</p>
-    </li>
-{/each}
-</ul>
+<div class="dashboard">
+    <div class="dashboard-header mb-m">
+        <a href="/private/scan-shelf" class="add-book">
+            <Icon icon = "icons8:plus" width={"72"} height={"72"}/>
+            <p>Add new book</p>
+        </a>
+    <div class="headline">
+        <h3 class="bold mb-xs">Welcome back, {userName}</h3>
+    </div>
+    </div>  
+</div>
+
+<style>
+    .dashboard-header{
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+        width: 100%;
+    }
+    .add-book{
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+    }
+    .add-book p
+    {
+        margin-left: 8px;
+    }
+    .add-book:hover
+    {
+        color: darkgray;
+        transition: linear 0.3s all;
+    }
+    .headline
+    {
+        text-align: right;
+        max-width: 30%;
+        min-width: 300px;
+    }
+</style>
