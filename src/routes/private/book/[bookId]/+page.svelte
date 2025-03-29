9d004fdc0ab28cd2d98d253f9ef859418929fa34
WKogut98 <wojciech.kogut7@gmail.com>
2025-03-29 18:29:48 +0100

    book update

--- src/routes/private/book/[bookId]/+page.svelte
+++ src/routes/private/book/[bookId]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import {Button, StarRating} from "$components";
-    import type { Book } from "$lib/state/user-state.svelte";
+    import { getUserState, type Book } from "$lib/state/user-state.svelte";
     import Icon from "@iconify/svelte";
 
     interface BookPageProps
@@ -10,20 +10,62 @@
         };
     }
 
+    let userContext = getUserState();
     let {data}:BookPageProps = $props();
-    let book = $derived(data.book);
+    let book = $derived(userContext.getBookById(data.book.id) || data.book);
+    let isEditMode = $state(false);
+
+    let title = $state(data.book.title);
+    let author = $state(data.book.author);
+    let description = $state(data.book.description || "");
+    let genre = $state(data.book.genre || "");
 
     function goBack()
     {
         history.back();
     }
+
+    async function toggleEditModeOrSave()
+    {
+        if(isEditMode)
+        {
+            await userContext.updateBook(book.id, {title, author, description, genre});
+        }
+        isEditMode = !isEditMode;
+    }
+
+    async function updateReadingStatus()
+    {
+        const startedReading = Boolean(book.started_reading_on);
+        if(startedReading)
+        {
+            await userContext.updateBook(book.id, {finished_reading_on: new Date().toISOString()});
+        }
+        else
+        {
+            await userContext.updateBook(book.id, {started_reading_on: new Date().toISOString()});
+        }
+    }
+
+    async function updateDatabaseRating(rating: number)
+    {
+        await userContext.updateBook(book.id, {rating});
+    }
 </script>
+
+{#snippet statusButtons()}
+{#if !book.finished_reading_on}
+<Button isSecondary={true} onclick={updateReadingStatus}>
+   {book.started_reading_on ? "I finished it" : "I've started reading it"}
+</Button>
+{/if}
+{/snippet}
 
 {#snippet bookInfo()}
 <h2 class = "book-title mt-m">{book.title}</h2>
 <p class = "book-author">{book.author}</p>
 <h4 class="mt-m mb-xs semi-bold">Your rating:</h4>
-<StarRating value={book.rating||0} />
+<StarRating value={book.rating||0} {updateDatabaseRating}/>
 <p class="small-font">Click to {book.rating ? "update rating" : "rate"}</p>
 {#if book.genre}
 <h4 class="mt-m mb-xs semi-bold">Genre</h4>
@@ -34,13 +76,28 @@
 <p class="mb-m">{book.description}</p>
 {:else}
 <p class="mb-m">No description yet</p>
-<button class = "block mb-m" onclick = {() =>console.log("Toggle edit mode")}>Add description</button>
+<button class = "block mb-m" onclick = {() => toggleEditModeOrSave()}>Add description</button>
 {/if}
-{#if !book.finished_reading_on}
-<Button isSecondary={true} onclick={() => console.log("update reading status")}>
-   {book.started_reading_on ? "I finished it" : "I've started reading it"}
-</Button>
-{/if}
+    {@render statusButtons()}
+{/snippet}
+
+{#snippet editFields()}
+    <form>
+        <input class = "input input-title mt-m mb-xs" bind:value={title} type="text" name="title"/>
+        <div class="input-author">
+            <p>by</p>
+            <input class = "input" bind:value={author} type="text" name="author"/>
+        </div>
+        <h4 class="mt-m mb-xs semi-bold">Your rating:</h4>
+        <StarRating value={book.rating||0} {updateDatabaseRating}/>
+        <p class="small-font">Click to {book.rating ? "update rating" : "rate"}</p>
+        <h4 class="mt-m mb-xs semi-bold">Genre</h4>
+        <input class = "input" bind:value={genre} type="text" name="genre"/>
+        <h4 class = "mt-m mb-xs semi-bold">Description</h4>
+        <textarea class = "textarea mb-m" name="description" bind:value={description} placeholder="Write a short summary of what the book is about.">
+        </textarea>
+        {@render statusButtons()}
+    </form>
 {/snippet}
 
 <div class="book-page">
@@ -49,7 +106,15 @@
     </button>
     <div class="book-container">
         <div class="book-info">
-            {@render bookInfo()}
+            {#if isEditMode}
+                {@render editFields()}
+            {:else}
+                {@render bookInfo()}
+            {/if}
+            <div class="buttons-container mt-m">
+                <Button isSecondary={true} onclick={()=>toggleEditModeOrSave()}>{isEditMode? "Save Changes" : "Edit"}</Button>
+                <Button isDanger={true} onclick={()=>console.log("Delete")}>Delete from library</Button>
+            </div>
         </div>
         <div class="book-cover">
             {#if book.cover_image}
@@ -83,7 +148,7 @@
         border: 1px solid black;
         border-radius: 15px;
         min-height: 400px;
-        max-width: 350px;
+        max-width: 450px;
         margin-left: 80px;
     }
     .book-cover img
@@ -99,5 +164,30 @@
         flex-direction: column;
         align-items: center;
         justify-content: center;
+    }
+
+    .input
+    {
+        padding: 8px 4 px;
+        width: 100%;
+    }
+    .textarea
+    {
+        width: 100%;
+    }
+    .input-title
+    {
+        font-size: 60px;
+        font-weight: bold;
+        font-family: "EB Garamond", serif;
+    }
+    .input-author
+    {
+        display: flex;
+        align-items: center;
+    }
+    .input-author p
+    {
+        margin-right: 8px;
     }
 </style>
