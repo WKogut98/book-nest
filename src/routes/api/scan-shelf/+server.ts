10a5af955630caff6f3929141e48dd1be9529d30
WKogut98 <wojciech.kogut7@gmail.com>
2025-04-11 21:12:44 +0200

    adding books part 1

---
+++ src/routes/api/scan-shelf/+server.ts
@@ -0,0 +1,8 @@
+import { json, type RequestHandler } from "@sveltejs/kit";
+
+export const POST: RequestHandler = async ({ request }) =>{
+    const {base64} = await request.json();
+
+    console.log("base64", base64);
+    return json({success: true, base64});
+}
---
+++ src/routes/private/scan-shelf/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import Dropzone from "svelte-file-dropzone";
+    import Icon from "@iconify/svelte";
+    import { convertFileToBase64 } from "$lib/utils/helpers";
+
+    let isLoading = $state(false);
+
+    async function handleDrop(e: CustomEvent<any>) {
+        const { acceptedFiles } = e.detail;
+        if (acceptedFiles.length) {
+            isLoading = true;
+            const fileToSend = acceptedFiles[0] as File;
+            const base64String = await convertFileToBase64(fileToSend);
+            try
+            {
+                const response = await fetch("api/scan-shelf", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    base64: base64String,})
+            });
+            console.log(`Response on the front-end: ${response}`);
+            }
+            catch (error) {
+                console.error("Error uploading file:", error);
+            }
+        }
+    }
+</script>
+
+<h2 class="mt-m mb-l">Take a picture to add books</h2>
+<div class="upload-area">
+    <div class="upload-container">
+        <Dropzone on:drop={()=>{handleDrop}} multiple={false} accept="image/*" maxSize{10*1024*1024}
+            containerClasses = {"dropzone-cover"}>
+            <Icon icon="bi:camera-fill" width="40px"/>
+            <p>Drop a picture of your shelf or select a file</p>
+        </Dropzone>
+    </div>
+</div>
