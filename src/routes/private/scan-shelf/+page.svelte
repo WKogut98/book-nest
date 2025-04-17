e61faea1d8a446933650121d822453effa311ded
WKogut98 <wojciech.kogut7@gmail.com>
2025-04-17 19:41:45 +0200

    Adding books part 1

--- src/routes/private/scan-shelf/+page.svelte
+++ src/routes/private/scan-shelf/+page.svelte
@@ -2,8 +2,12 @@
     import Dropzone from "svelte-file-dropzone";
     import Icon from "@iconify/svelte";
     import { convertFileToBase64 } from "$lib/utils/helpers";
+    import {Button} from "$components";
 
     let isLoading = $state(false);
+    let errorMessage = $state("");
+    let recognizedBooks = $state<OpenAIBook[]>([]);
+    let booksAddSuccess = $state(false);
 
     interface OpenAIBook
     {
@@ -29,23 +33,71 @@
             });
 
             const result = await response.json() as {bookArray: OpenAIBook[]};
+
+            recognizedBooks = result.bookArray;
             console.log(result);
             console.log(`Response on the front-end: ${response}`);
+            isLoading = false;
             }
             catch (error) {
-                console.error("Error uploading file:", error);
+                errorMessage = "An error occurred while processing the image. Please try again.";
             }
+        }
+        else
+        {
+            errorMessage = "Could not upload the image. Please try again. Images have to be smaller than 10MB.";
         }
     }
 </script>
 
 <h2 class="mt-m mb-l">Take a picture to add books</h2>
+{#if recognizedBooks.length===0}
 <div class="upload-area">
     <div class="upload-container">
+        {#if errorMessage}
+           <h4 class="text-center mb-s upload-error">{errorMessage}</h4>>
+        {/if}
+        {#if isLoading}
+            <div class="spinner-container">
+                <div class="spinner"></div>
+                <p class="text-center">Processing image...</p>
+            </div>
+            
+        {:else}
         <Dropzone on:drop={handleDrop} multiple={false} accept="image/*" maxSize={10*1024*1024}
             containerClasses = {"dropzone-cover"}>
             <Icon icon="bi:camera-fill" width="40px"/>
             <p>Drop a picture of your shelf or select a file</p>
         </Dropzone>
+        {/if}
     </div>
 </div>
+{:else if !booksAddSuccess}
+    <div class="found-books">
+        <table class="book-list mb-m">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each recognizedBooks as book, i}
+                    <tr>
+                        <td>{book.bookTitle}</td>
+                        <td>{book.author}</td>
+                        <td><button type="button" aria-label="Remove book" class="remove-book"
+                            onclick={()=>console.log(`Delete book with index ${i}`)}>
+                            <Icon icon="streamline:delete-1-solid" width="24"/>
+                        </button></td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+        <Button onclick={()=>console.log("Add books to library")}>Add all</Button>
+    </div>
+{:else}
+    <h4>The selected {recognizedBooks,length} books have been added</h4>
+    <Button href="/private/dashboard">Go back</Button>
+{/if}
